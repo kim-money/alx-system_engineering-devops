a3cad9e82d2e792fd319871f8f2b177935379766
moses_kim <wangingimoses102@gmail.com>
2024-03-20 10:49:11 +0300

    debug3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fixes 500 error on the Apache web server
+
+exec {'replace':
+    provider => shell,
+    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,3 @@
+0x17-web_stack_debugging_3-    
+
+install tmux and learnt how to use it, use strace to debug
